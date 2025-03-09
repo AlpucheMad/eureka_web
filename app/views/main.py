d7f3ad4a74734b47da611d009ad0eae8cbadacfd
Gerardo Alpuche <alpuchebado@outlook.com>
2025-03-09 00:43:46 -0600

    feat(auth): Implementar redirección a login para usuarios no autenticados

--- app/views/main.py
+++ app/views/main.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('auth.login'))
+    return render_template('index.html', now=datetime.now())
